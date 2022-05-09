e728d04c882473944e166a8f6376c0f63f84cd0e
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-09 22:19:56 +0300

    test instances

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -9,3 +9,19 @@ class BlogTest(unittest.TestCase):
         '''
         self.user_admin = User(username='admin',password_hash='admin',email='admin@admin.com')
         self.new_article = Article(article='article1',category='technology',user_id=self.user_admin)
+
+    def tearDown(self):
+        '''
+        deletes test data tests after every test
+        '''
+        User.query.delete()
+        Article.query.delete()
+
+    def test_check_instance_variables(self):
+        '''
+        test the instances
+        '''
+        self.assertEquals(self.new_article.article,'article1')
+        self.assertEquals(self.new_article.category,'technology')
+        self.assertEquals(self.new_article.user_id,self.user_admin)
+                
