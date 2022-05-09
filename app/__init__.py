3857486687bcceb7ff5b13a8fea18d24e32f9cf3
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-09 22:47:01 +0300

    data structure

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_uploads import configure_uploads,IMAGES,UploadSet
+
+app = Flask(__name__)
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
