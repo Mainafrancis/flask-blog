e2631bd4c7c21ce9d29b6e47fdeeecc7998ec00c
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-09 22:08:33 +0300

    manage shell

--- manage.py
+++ manage.py
@@ -19,4 +19,10 @@ def test():
     test = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=5).run(tests)
 
-    
+@manager.shell
+def make_shell_context():
+    return {'db' : db,'User':User , 'Article':Article}
+
+
+if __name__=="__main__":
+    manager.run()        
