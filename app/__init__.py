c1c0abe60dc0f340d0b62240dc9a52ac38812219
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-09 22:50:50 +0300

    import blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,15 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+
+def create_app(configname):
+    '''
+    Method for creating the app to enable for easier configurations
+    '''
+    app.config.from_object(configurations[configname])
+
+    #Importing BluePrints
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
