996953a1ebbbf2819b4f64188cdb482035211d05
Mainafrancis <francis.maina@student.moringaschool.com>
2022-05-09 22:14:50 +0300

    self-alliance

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,4 +4,8 @@ from app import db
 
 class BlogTest(unittest.TestCase):
     def setUp(self):
-        
+        '''
+        Sets up the before all tests
+        '''
+        self.user_admin = User(username='admin',password_hash='admin',email='admin@admin.com')
+        self.new_article = Article(article='article1',category='technology',user_id=self.user_admin)
